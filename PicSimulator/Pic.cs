64b45beda5a6ba62e50cf9da4954cc6619d6ed58
Jonas <jonas.gerstl@outlook.com>
2024-04-17 18:11:55 +0200

    Laden Knopf funktioniert

--- PicSimulator/Pic.cs
+++ PicSimulator/Pic.cs
@@ -32,25 +32,42 @@ namespace PicSimulator
                 OnPropertyChanged(nameof(Code));
             }
         }
+        private string testString = "blabla";
+        public string TestString
+        {
+            get { return testString; }
+            set
+            {
+                testString = value;
+                OnPropertyChanged(nameof(TestString));
+            }
+        }
 
 
 
         public Pic()
         {
-            LoadFile();
+            //Load("C:\\Users\\jonas\\Downloads\\TestProg_PicSim_20230413\\TPicSim1.LST");
         }
-        private void LoadFile()
+        public void LoadFile()
+        {
+            Load("C:\\Users\\jonas\\Downloads\\TestProg_PicSim_20230413\\TPicSim1.LST");
+        }
+        public void ChangeString()
+        {
+            TestString = "Hallo";
+        }
+        private void Load(string path)
         {
             try
             {
-                using (var sr = new StreamReader("C:\\Users\\jonas\\Downloads\\TestProg_PicSim_20230413\\TPicSim1.LST", Encoding.Default))
+                using (var sr = new StreamReader(path, Encoding.Default))
                 {
                     while (!sr.EndOfStream)
                     {
                         Match match = Regex.Match(sr.ReadLine(), @"^ *(([0-9A-F]{4}) ([0-9A-F]{4}) *)?([0-9]{5}) *(.*)$");
                         if(match.Success)
                         {
-
                             Code.Add(match.Groups[4].Value + "    " + match.Groups[5].Value);
                             if (match.Groups[1].Success)
                             {
--- PicSimulator/PicViewModel.cs
+++ PicSimulator/PicViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace PicSimulator
@@ -14,42 +15,73 @@ namespace PicSimulator
         public PicViewModel()
         {
             pic = new Pic();
-            pic.PropertyChanged += Pic_PropertyChangd;
+            pic.PropertyChanged += Pic_PropertyChanged;
             //Commands
-            //ButtonClick = new RelayCommand(_ => Click());
+            LoadFileCommand = new RelayCommand(_ => LoadFileButton());
         }
-        private void Pic_PropertyChangd(object sender, PropertyChangedEventArgs e)
+        private void Pic_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             OnPropertyChanged(e.PropertyName);
         }
 
         //Variablen
+        private List<int> hexCode;
         public List<int> HexCode
         {
-            get { return pic.HexCode; }
+            get { return hexCode; }
             set
             {
-                pic.HexCode = value;
+                hexCode = value;
                 OnPropertyChanged(nameof(HexCode));
             }
         }
+        private List<string> code;
         public List<string> Code
         {
-            get { return pic.Code; }
+            get { return code; }
             set
             {
-                pic.Code = value;
+                code = value;
                 OnPropertyChanged(nameof(Code));
             }
         }
-        //Commands
-        //public ICommand ButtonClick { get; }
-        //public void Click()
-        //{
-        //    pic.Click();
-        //    OnPropertyChanged(nameof(TestString));
-        //}
 
+        private string testString;
+        public string TestString
+        {
+            get { return testString; }
+            set
+            {
+                testString = value;
+                OnPropertyChanged(nameof(TestString));
+            }
+        }
+        //Commands
+        public ICommand LoadFileCommand { get; }
+        public void LoadFileButton()
+        {
+            pic.LoadFile();
+            pic.ChangeString();
+            TestString = pic.TestString;
+            Code = pic.Code;
+        }
+
+        //PropertyChanged
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(pic.HexCode):
+                    HexCode = pic.HexCode;
+                    break;
+                case nameof(pic.Code):
+                    Code = pic.Code;
+                    break;
+                case nameof(pic.TestString):
+                    TestString = pic.TestString;
+                    break;
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
         {
