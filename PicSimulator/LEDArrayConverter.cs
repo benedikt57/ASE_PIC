f548e363b8e20fb34021fc198767d16df6cd3125
Skyrim2 <sebbel200@gmail.com>
2024-05-06 14:46:04 +0200

    LED Array Implementiert, läft nur auf RB

---
+++ PicSimulator/LEDArrayConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PicSimulator
+{
+    public class LEDArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is string str)
+            {
+                if ((value is int intValue))
+                {
+                    if((intValue & (1 << int.Parse(str))) != 0)
+                    {
+                        return Brushes.Red;
+                    }
+                    else
+                    {
+                        return Brushes.Gray;
+                    }
+                }
+            }
+            return Brushes.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PicSimulator/PicViewModel.cs
+++ PicSimulator/PicViewModel.cs
@@ -13,6 +13,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace PicSimulator
 {
