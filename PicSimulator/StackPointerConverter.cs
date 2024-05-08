5dd534b4115b4be48568b7319f8f5ffc96a4eba2
Jonas <jonas.gerstl@outlook.com>
2024-05-08 18:23:06 +0200

    Stackpointer zeigt auf Stack

---
+++ PicSimulator/StackPointerConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PicSimulator
+{
+    public class StackPointerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue && parameter is string strPara && intValue == int.Parse(strPara))
+            {
+                return System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                return System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
