ff7f58e81f0275fd9815290000fd71a67f17b802
Jonas <jonas.gerstl@outlook.com>
2025-03-29 13:04:30 +0100

    Hinzufügen von Decodern für einzelne Commands

---
+++ Application/Decoder.cs
@@ -0,0 +1,37 @@
+﻿using PicSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class GotoDecoder : IDecoder
+    {
+        public bool Decode(int code, ICommands commands)
+        {
+            int opcode = code & 0b0011_1000_0000_0000;
+            if (opcode == 0b0010_1000_0000_0000)
+            {
+                commands.GOTO(code & 0b0000_0111_1111_1111);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class CallDecoder : IDecoder
+    {
+        public bool Decode(int code, ICommands commands)
+        {
+            int opcode = code & 0b0011_1000_0000_0000;
+            if (opcode == 0b0010_0000_0000_0000)
+            {
+                commands.CALL(code & 0b0000_0111_1111_1111);
+                return true;
+            }
+            return false;
+        }
+    }
+}
