d6946ce447ed07cb8ecd2a53a3a47fa289ebec72
Jonas <jonas.gerstl@outlook.com>
2025-03-29 13:00:40 +0100

    IDecoder Interface hinzufügen

---
+++ Application/IDecoder.cs
@@ -0,0 +1,14 @@
+﻿using PicSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    interface IDecoder
+    {
+        bool Decode(int code, ICommands commands);
+    }
+}
