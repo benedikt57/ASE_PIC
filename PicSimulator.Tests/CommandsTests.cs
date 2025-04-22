3b29e3749abe97e7dff0252076cd90413a462e0b
Jonas <jonas.gerstl@outlook.com>
2025-04-22 22:24:00 +0200

    Hinzufügen von moq und dem ersten Test

--- PicSimulator.Tests/CommandsTests.cs
+++ PicSimulator.Tests/CommandsTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace PicSimulator.Tests
 {
     public class CommandsTests
     {
+        [Fact]
+        public void MOVLW_ShouldSetWRegToLiteral()
+        {
+            // Arrange
+            var mockPic = new Mock<IPic>();
+            //int wRegValue = 0;
+            //var ram = new int[256];
+            mockPic.SetupProperty(p => p.WReg, 0);
+            mockPic.SetupProperty(p => p.Ram, new int[256]);
+            var commands = new Commands(mockPic.Object);
+
+            // Act
+            commands.MOVLW(42);
+
+            // Assert
+            Assert.Equal(42, mockPic.Object.WReg);
+        }
     }
 }
