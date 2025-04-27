13683a1266c465aeaff3f7c89ac6aaabb653f9e6
Jonas <jonas.gerstl@outlook.com>
2025-04-27 17:01:34 +0200

    DECFSZ Tests

--- PicSimulator.Tests/CommandsTests.cs
+++ PicSimulator.Tests/CommandsTests.cs
@@ -15,8 +15,6 @@ namespace PicSimulator.Tests
         {
             // Arrange
             var mockPic = new Mock<IPic>();
-            //int wRegValue = 0;
-            //var ram = new int[256];
             mockPic.SetupProperty(p => p.WReg, 0);
             mockPic.SetupProperty(p => p.Ram, new int[256]);
             var commands = new Commands(mockPic.Object);
@@ -26,6 +24,56 @@ namespace PicSimulator.Tests
 
             // Assert
             Assert.Equal(42, mockPic.Object.WReg);
+        }
+
+        [Fact]
+        public void DECFSZ_ShouldDecrementAndWriteBackToFile()
+        {
+            // Arrange
+            var mockPic = new Mock<IPic>();
+            mockPic.SetupProperty(p => p.Ram, new int[256]);
+            mockPic.Object.Ram[0x10] = 42;
+            var commands = new Commands(mockPic.Object);
+
+            //Act
+            commands.DECFSZ(0b1001_0000);
+
+            //Assert
+            Assert.Equal(41, mockPic.Object.Ram[0x10]);
+        }
+
+        [Fact]
+        public void DECFSZ_ShouldDecrementAndWriteToWReg()
+        {
+            // Arrange
+            var mockPic = new Mock<IPic>();
+            mockPic.SetupProperty(p => p.WReg, 0);
+            mockPic.SetupProperty(p => p.Ram, new int[256]);
+            mockPic.Object.Ram[0x10] = 42;
+            var commands = new Commands(mockPic.Object);
+
+            //Act
+            commands.DECFSZ(0b0001_0000);
+
+            //Assert
+            Assert.Equal(41, mockPic.Object.WReg);
+        }
+
+        [Fact]
+        public void DECFSZ_ShouldSkip()
+        {
+            // Arrange
+            var mockPic = new Mock<IPic>();
+            mockPic.SetupProperty(p => p.Ram, new int[256]);
+            mockPic.SetupProperty(p => p.PC, 20);
+            mockPic.Object.Ram[0x10] = 1;
+            var commands = new Commands(mockPic.Object);
+
+            //Act
+            commands.DECFSZ(0b1001_0000);
+
+            //Assert
+            Assert.Equal(21, mockPic.Object.PC);
         }
     }
 }
